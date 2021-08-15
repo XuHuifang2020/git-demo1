cde6d4d5e14f860ee7f87927f70bb1ace6db1367
huifang_xu@outlook.com <983319682@qq.com>
2021-08-16 02:35:34 +0800

    hot-fix 01

--- src/main/java/test/August.java
+++ src/main/java/test/August.java
@@ -5,5 +5,6 @@ public class August {
         System.out.println("first");
         System.out.println("second");
         System.out.println("third");
+        System.out.println("hot-fix");
     }
 }
