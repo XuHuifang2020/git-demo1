9aca88f2ac82e116c193270b4908905c46221264
huifang_xu@outlook.com <983319682@qq.com>
2021-08-16 02:51:05 +0800

    00

--- src/main/java/test/August.java
+++ src/main/java/test/August.java
@@ -7,5 +7,6 @@ public class August {
         System.out.println("third");
         System.out.println("hot-fix");
         System.out.println("master test");
+        System.out.println("hot-fix test");
     }
 }
