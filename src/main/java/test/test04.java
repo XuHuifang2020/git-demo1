4e1c4729569ea042bd841f83c083d345ed903b79
huifang_xu@outlook.com <983319682@qq.com>
2021-08-16 22:44:45 +0800

    commit 03

--- src/main/java/test/test04.java
+++ src/main/java/test/test04.java
@@ -1,4 +1,15 @@
 package test;
 
 public class test04 {
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+        System.out.println("hot-fix");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("remote test");
+    }
 }
